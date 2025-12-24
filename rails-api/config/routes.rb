7a47d378a4e0fe752cc3bac545b038ed74ae4146
Sachin Maharjan <sachin9860810@gmail.com>
2025-12-24 23:45:30 +0545

    feat: scope rails under /api and add ingress in kubernetes cluster

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :metrics, only: [:index, :create] do
-    collection do
-      get :latest    # /metrics/latest
-      get :stats     # /metrics/stats
+  scope :api do
+    resources :metrics, only: %i[index create] do
+      collection do
+        get :latest    # /metrics/latest
+        get :stats     # /metrics/stats
+      end
     end
+
+    # Health check endpoint
+    get :health, to: ->(_env) { [200, {}, ['OK']] }
   end
 
-  # Health check endpoint
-  get :health, to: ->(env) { [200, {}, ['OK']] }
 
   # Root endpoint
-  root to: ->(env) { [200, {}, ['Metrics API']] }
+  root to: ->(_env) { [200, {}, ['Metrics API']] }
 end
